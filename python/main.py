5985d47df345f4ff692d7478e1e316bdb18d54b9
wairzox <wairzox@gmail.com>
2024-03-18 09:52:35 +0100

    Creation base de données + réorganisation du projet

--- python/main.py
+++ python/main.py
@@ -1,29 +1,46 @@
-from flask import Flask, render_template, redirect, url_for, request
-import pandas as pd
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
-users = pd.read_csv('User.csv')['user'].tolist()
-passwords = pd.read_csv('User.csv')['password'].tolist()
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}')"
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    books = Book.query.all()
+    return render_template('index.html', books=books)
 
-@app.route('/login', methods=['POST'])
-def login():
-    user = request.form['user']
-    password = request.form['password']
+@app.route('/delete-all')
+def delete_all():
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+    return redirect(url_for('index'))
 
-    # Vérifiez si les informations d'identification sont valides
-    if user in users and passwords[users.index(user)] == password:
-        return redirect(url_for('home'))
-    else:
-        return 'Invalid credentials'
-
-@app.route('/home')
-def home():
-    return 'Welcome to the home page!'
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        new_book = Book(title=title, author=author)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('create.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+        # Ajoutez des données à la base dedonnées ici si vous le souhaitez
+        new_book = Book(title="1984", author="George Orwell")
+        db.session.add(new_book)
+        db.session.commit()
+        app.run(debug=True)

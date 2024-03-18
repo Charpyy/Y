4847f54fc49a3c8d2eda2f827e5ee88f9f156eec
wairzox <wairzox@gmail.com>
2024-03-18 10:52:09 +0100

    Ajout de la fonction inscripton/login

--- python/main.py
+++ python/main.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+import secrets
+import bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.secret_key = secrets.token_hex(16)
 db = SQLAlchemy(app)
 
 class Book(db.Model):
@@ -13,19 +17,32 @@ class Book(db.Model):
     def __repr__(self):
         return f"Book('{self.title}', '{self.author}')"
 
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}')"
+
+    def set_password(self, password):
+        self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 @app.route('/')
+@login_required
 def index():
     books = Book.query.all()
-    return render_template('index.html', books=books)
-
-@app.route('/delete-all')
-def delete_all():
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-    return redirect(url_for('index'))
+    return render_template('index.html',books=books)
 
 @app.route('/create', methods=['GET', 'POST'])
+@login_required
 def create():
     if request.method == 'POST':
         title = request.form['title']
@@ -36,11 +53,46 @@ def create():
         return redirect(url_for('index'))
     return render_template('create.html')
 
-if __name__ == '__main__':
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.checkpw(password.encode('utf-8'), user.password):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            return 'Invalid username or password'
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        new_user = User(username=username)
+        new_user.set_password(password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+if __name__ =='__main__':
     with app.app_context():
         db.create_all()
-        # Ajoutez des données à la base dedonnées ici si vous le souhaitez
-        new_book = Book(title="1984", author="George Orwell")
-        db.session.add(new_book)
+        # Ajoutez des données à la base de données ici si vous le souhaitez
+        '''new_book = Book(title="1984", author="George Orwell")
+        db.session.add(new_book)'''
         db.session.commit()
         app.run(debug=True)
